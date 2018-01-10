118d792acba056fb43eb72a3f2610849873c095f
usrprdprovad <usrprdprovad@upc.edu.pe>
2018-01-10 15:48:40 -0500

    CAP4

---
+++ CAP4/DesignMethods/MethodSignature.java
@@ -0,0 +1,18 @@
+
+public class MethodSignature{
+	
+	public static void main(String args[]){
+	}
+
+	//Acces modifier: private, default, protected, public (no required)
+	//Optional specifier: final, static, abstract, final, synchronized, native, strictfp (no required)
+	//Return type: void (required)
+	//Method name: nap (required)
+	//Paramterers: between parentheses (required, but can be empty parantheses)
+	//Exception: example: InterruptionException (no required)
+
+	//public final void nap(int minutes) throws InterruptionException{
+		
+	//}
+
+}
---
+++ CAP4/DesignMethods/SyntaxMethod.java
@@ -0,0 +1,14 @@
+public class SyntaxMethod{
+	public static void main(String args[]){
+				
+
+	}
+	public void walk(){}
+	public final void walk2(){}
+	public static final void walk3(){}
+	public final static void walk4(){}
+	//public modifier void walk5(){} // DOES NOT COMPILE
+	//public void final walk6(){} // DOES NOT COMPILE
+	final public void walk7(){}
+
+}
