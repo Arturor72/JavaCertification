1272a19c0abd03f2eca7c107cb5822d09563814a
arturor72 <arturor72.jar@gmail.com>
2018-02-03 22:03:42 -0600

    clases abstractas

---
+++ CAP5/Inheritance/AbstractClass/BigCat.java
@@ -0,0 +1,7 @@
+//Una clase abstracta puede extender de otra clase abstracta y no implementar el metodo abstracta
+//La primera clase concreta que extendia de una clase abstracta tiene que implementar todos los metodos 
+//de las ancestras clases abstractas
+public abstract class BigCat extends Animal{
+	public abstract void roar();
+
+}
---
+++ CAP5/Inheritance/AbstractClass/Lion.java
@@ -0,0 +1,10 @@
+public class Lion extends Bigcat{
+	public String getName(){
+		return "Lion";
+	
+	}
+	public void roar(){
+		System.out.println("The lion lets out a loud ROAR!!");
+	}
+
+}
