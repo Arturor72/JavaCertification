b78d398c71306c2c166534f2c2067ec371fbf448
Arturo <arturor72.jar@gmail.com>
2018-02-04 21:41:47 -0500

    access classs

--- CAP5/Access/Laptop.java
+++ CAP5/Access/Laptop.java
@@ -1,5 +1,6 @@
 //public class Computer{  // Solo una clase puede tener el modificador de acceso publico
 // en un Java file, y es asi como se tiene que nombrar el file
+// Una clase solo puede tener como modificador de acceso publico o default(package private)
 class Computer{
 
 }
